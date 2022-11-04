0c8c51e2a911530a2cbc54fa2813a6ccb2a22ac6
unknown <kucherenko.alexandr1998@gmail.com>
2022-11-04 03:03:57 +0300

    222

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default class MyDocument extends Document {
         return(
             <Html>
                 <Head>
+                    <title>Puzzle - P2P Арбитраж</title>
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
                     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/> 
